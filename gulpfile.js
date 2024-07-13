ab4cfb0a21d3cbb52120f370d4d298f1f8743abb
JoelRevington1305 <joelrevington28@gmail.com>
2024-07-13 13:49:52 +0530

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,76 +1,68 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const autoprefixer = require('gulp-autoprefixer');
-const imagemin = require('gulp-imagemin');
-const pngquant = require('imagemin-pngquant');
-const cache = require('gulp-cache');
-const cp = require('child_process');
-const browserSync = require('browser-sync').create();
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var cache = require('gulp-cache');
+var cp = require('child_process');
+var browserSync = require('browser-sync');
 
-const jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 
 // Build the Jekyll Site
-function jekyllBuild(done) {
-  return cp.spawn(jekyll, ['build'], { stdio: 'inherit' }).on('close', done);
-}
+gulp.task('jekyll-build', function (done) {
+    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+        .on('close', done);
+});
 
 // Rebuild Jekyll and page reload
-function jekyllRebuild(done) {
-  browserSync.reload();
-  done();
-}
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
 
 // Wait for jekyll-build, then launch the Server
-function serve(done) {
-  browserSync.init({
-    server: {
-      baseDir: '_site'
-    },
-    notify: false
-  });
-  done();
-}
+gulp.task('browser-sync', ['sass', 'img', 'jekyll-build'], function() {
+    browserSync({
+        server: {
+            baseDir: '_site'
+        },
+        notify: false
+    });
+});
 
 // Compile files
-function compileSass() {
-  return gulp.src('assets/css/scss/main.scss')
-    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-    .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-    .pipe(gulp.dest('_site/assets/css'))
-    .pipe(browserSync.stream())
-    .pipe(gulp.dest('assets/css'));
-}
+gulp.task('sass', function () {
+    return gulp.src('assets/css/scss/main.scss')
+        .pipe(sass({
+            outputStyle: 'expanded',
+            onError: browserSync.notify
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('_site/assets/css'))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest('assets/css'));
+});
 
 // Compression images
-function compressImages() {
-  return gulp.src('assets/img/**/*')
-    .pipe(cache(imagemin({
-      interlaced: true,
-      progressive: true,
-      svgoPlugins: [{ removeViewBox: false }],
-      use: [pngquant()]
-    })))
+gulp.task('img', function() {
+	return gulp.src('assets/img/**/*')
+		.pipe(cache(imagemin({
+			interlaced: true,
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		})))
     .pipe(gulp.dest('_site/assets/img'))
-    .pipe(browserSync.stream());
-}
+    .pipe(browserSync.reload({stream:true}));
+});
 
 // Watch scss, html, img files
-function watchFiles() {
-  gulp.watch('assets/css/scss/**/*.scss', compileSass);
-  gulp.watch('assets/js/**/*.js', gulp.series(jekyllBuild, jekyllRebuild));
-  gulp.watch('assets/img/**/*', compressImages);
-  gulp.watch(['*.html', '_layouts/*.html', '_includes/*.html', '_pages/*.html', '_posts/*'], gulp.series(jekyllBuild, jekyllRebuild));
-}
+gulp.task('watch', function () {
+    gulp.watch('assets/css/scss/**/*.scss', ['sass']);
+    gulp.watch('assets/js/**/*.js', ['jekyll-rebuild']);
+    gulp.watch('assets/img/**/*', ['img']);
+    gulp.watch(['*.html', '_layouts/*.html', '_includes/*.html', '_pages/*.html', '_posts/*'], ['jekyll-rebuild']);
+});
 
-// Define complex tasks
-const build = gulp.series(jekyllBuild, gulp.parallel(compileSass, compressImages));
-const watch = gulp.parallel(watchFiles, serve);
-
-// Export tasks
-exports.sass = compileSass;
-exports.img = compressImages;
-exports.jekyllBuild = jekyllBuild;
-exports.jekyllRebuild = jekyllRebuild;
-exports.build = build;
-exports.watch = watch;
-exports.default = watch;
+//  Default task
+gulp.task('default', ['browser-sync', 'watch']);
